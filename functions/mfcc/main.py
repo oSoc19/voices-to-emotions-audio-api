2a1ab131a9e2180c5907565448088fceb395b7f9
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-16 19:50:02 +0200

    update AI to v2

--- functions/mfcc/main.py
+++ functions/mfcc/main.py
@@ -7,7 +7,9 @@ from flask import jsonify
 from googleapiclient import discovery
 
 ALLOWED_EXTENSIONS = ['aiff', 'wav', 'mp3']
-MFCC_FEATURES = 20
+MFCC_FEATURES = 12
+MFCC_LENGTH = 200
+SILENCE_TRESHOLD = 50
 
 emotion_dict = {
     0: 'neutral',
@@ -58,7 +60,7 @@ def map_predictions(predictions):
 def get_predictions(instances):
     project = 'voices-to-emotions'
     model = 'emotionrecognition'
-    version = 'v1_1'
+    version = 'v2'
 
     service = discovery.build('ml', 'v1')
     name = 'projects/{}/models/{}'.format(project, model)
@@ -80,7 +82,7 @@ def get_predictions(instances):
 def load_audio_data(file_path):
     # 16000 Hz = VoIP
     wave, sr = librosa.load(file_path, mono=True, sr=16000)
-    wave_frag_offsets = librosa.effects.split(wave, top_db=35)
+    wave_frag_offsets = librosa.effects.split(wave, top_db=SILENCE_TRESHOLD)
 
     results = []
     timestamps = []
@@ -93,13 +95,14 @@ def load_audio_data(file_path):
         mfcc = librosa.feature.mfcc(wave_fragment, sr, n_mfcc=MFCC_FEATURES)
         _, y_size = mfcc.shape
 
-        splitted_mfcc = np.array_split(mfcc, math.ceil(y_size / 500), axis=1)
+        splitted_mfcc = np.array_split(mfcc, math.ceil(y_size / MFCC_LENGTH), axis=1)
 
         one_mfcc_secs = frag_duration / y_size
 
         for short_mfcc in splitted_mfcc:
             mfcc_len = len(short_mfcc[0])
-            short_mfcc = np.pad(short_mfcc, ((0, 0), (0, 500 - len(short_mfcc[0]))), mode='constant', constant_values=0)
+            short_mfcc = np.pad(short_mfcc, ((0, 0), (0, MFCC_LENGTH - len(short_mfcc[0]))), mode='constant',
+                                constant_values=0)
             results.append(np.array(short_mfcc).tolist())
 
             duration = one_mfcc_secs * mfcc_len

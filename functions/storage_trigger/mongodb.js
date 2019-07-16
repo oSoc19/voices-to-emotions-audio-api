fc1067936488cc088aa9d9274091b6d20c71c3e7
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-16 14:21:11 +0200

    fix db issues

--- functions/storage_trigger/mongodb.js
+++ functions/storage_trigger/mongodb.js
@@ -4,33 +4,28 @@ const ObjectId = Schema.ObjectId;
 
 module.exports = async function addDataEntry(dataEntry) {
   mongoose.connect(process.env.MONGODB_CONN_STRING, { useNewUrlParser: true });
-  let db = mongoose.connection;
-  await new Promise((resolve, reject) => {
-    db.once('error', e => reject(e));
-    db.once('open', () => resolve());
-  });
 
-  let schema = new Schema({
-    user_id: ObjectId,
-    created: Date,
-    emotions: [
-      {
-        angry: Number,
-        calm: Number,
-        disgust: Number,
-        fearful: Number,
-        happy: Number,
-        neutral: Number,
-        sad: Number,
-        surprised: Number
-      }
-    ],
-    timestamps: [[Number, Number]]
-  });
+  let Model = mongoose.model(
+    "DataEntry",
+    new Schema({
+      user_id: ObjectId,
+      created: Date,
+      emotions: [
+        {
+          angry: Number,
+          calm: Number,
+          disgust: Number,
+          fearful: Number,
+          happy: Number,
+          neutral: Number,
+          sad: Number,
+          surprised: Number
+        }
+      ],
+      timestamps: [[Number, Number]]
+    })
+  );
 
-  let Model = mongoose.model("DataEntry", schema);
-
-  Model.insertMany([dataEntry])
-
+  await Model.insertMany([dataEntry]);
   await mongoose.connection.close();
-}
+};

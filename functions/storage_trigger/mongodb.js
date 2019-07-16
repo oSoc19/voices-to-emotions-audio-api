8c18280f85c27d14b63f3c669bbb6b3a34ece8b4
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-16 14:32:01 +0200

    magic

--- functions/storage_trigger/mongodb.js
+++ functions/storage_trigger/mongodb.js
@@ -2,30 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
+mongoose.connect(process.env.MONGODB_CONN_STRING, { useNewUrlParser: true });
+
+let Model = mongoose.model(
+  "DataEntry",
+  new Schema({
+    user_id: ObjectId,
+    created: Date,
+    emotions: [
+      {
+        angry: Number,
+        calm: Number,
+        disgust: Number,
+        fearful: Number,
+        happy: Number,
+        neutral: Number,
+        sad: Number,
+        surprised: Number
+      }
+    ],
+    timestamps: [[Number, Number]]
+  })
+);
+
 module.exports = async function addDataEntry(dataEntry) {
-  mongoose.connect(process.env.MONGODB_CONN_STRING, { useNewUrlParser: true });
-
-  let Model = mongoose.model(
-    "DataEntry",
-    new Schema({
-      user_id: ObjectId,
-      created: Date,
-      emotions: [
-        {
-          angry: Number,
-          calm: Number,
-          disgust: Number,
-          fearful: Number,
-          happy: Number,
-          neutral: Number,
-          sad: Number,
-          surprised: Number
-        }
-      ],
-      timestamps: [[Number, Number]]
-    })
-  );
-
   await Model.insertMany([dataEntry]);
   await mongoose.connection.close();
 };

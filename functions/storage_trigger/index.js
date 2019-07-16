eed0553d938cc3c4ee9206dd0c237a0db26c4e12
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-16 11:43:53 +0200

    bucket to db

--- functions/storage_trigger/index.js
+++ functions/storage_trigger/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const https = require("https");
 const rp = require("request-promise-native");
+const addDataEntry = require("./mongodb");
 
 const EXTENSIONS = [".wav", ".mp3", ".aiff"];
 const BUCKET_NAME = "voices-to-emotions-call-data";
@@ -15,11 +16,31 @@ exports.storage_trigger = async (data, context) => {
 
   console.log(process.env.MONGODB_CONN_STRING);
 
+  let fileparts = filename.split("/");
+  let user_id = fileparts[0];
+  let timestamp = fileparts.split("-")[0];
+
+  console.log({ user_id, timestamp });
+
+  if (!user_id || isNaN(timestamp)) {
+    throw new Error("Invalid file format");
+    return;
+  }
+
   var options = {
     uri: `https://europe-west1-voices-to-emotions.cloudfunctions.net/mfcc?uri=https://storage.googleapis.com/${BUCKET_NAME}/${filename}`,
     json: true
   };
 
   let response = await rp(options);
-  console.log({ response });
+  if (response.type === "success") {
+    let data = response.data;
+
+    addDataEntry({
+      emotions: data.emotions,
+      timestamps: data.timestamps,
+      created: new Date(timestamp),
+      user_id
+    });
+  }
 };
---
+++ functions/storage_trigger/mongodb.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+mongoose.connect(process.env.MONGODB_CONN_STRING, { useNewUrlParser: true });
+
+export default function addDataEntry(dataEntry) {
+  let db = mongoose.connection;
+  await new Promise((resolve, reject) => {
+    db.once('error', e => reject(e));
+    db.once('open', () => resolve());
+  });
+
+  let schema = new Schema({
+    user_id: ObjectId,
+    created: Date,
+    emotions: [
+      {
+        angry: Number,
+        calm: Number,
+        disgust: Number,
+        fearful: Number,
+        happy: Number,
+        neutral: Number,
+        sad: Number,
+        surprised: Number
+      }
+    ],
+    timestamps: [[Number, Number]]
+  });
+
+  let Model = mongoose.model("DataEntry", schema);
+
+  Model.insertMany([dataEntry])
+}
